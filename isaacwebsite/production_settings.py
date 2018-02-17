4d51515e3190e98b4ad48fa122f3c5bb68919a39
Isaac Layton <isaacdlayton@gmail.com>
2018-02-16 18:29:24 -0800

    add domain to allowed hosts

--- isaacwebsite/production_settings.py
+++ isaacwebsite/production_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY_SETTING']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['SERVER_SETTING'], '127.0.0.1']
+ALLOWED_HOSTS = [os.environ['SERVER_SETTING'], 'isaaclayton.com', 'www.isaaclayton.com']
 
 # Application definition
 
