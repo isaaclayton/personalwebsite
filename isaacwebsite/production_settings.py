3e552b78c2539491589079cedc8803f86de295f1
Isaac Layton <isaacdlayton@gmail.com>
2018-02-16 19:04:59 -0800

    add google analytics

--- isaacwebsite/production_settings.py
+++ isaacwebsite/production_settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+GA_TRACKING_ID = os.environ['GA_TRACKING_ID']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'isaacwebsite.context_processors.ga_tracking_id'
             ],
         },
     },
